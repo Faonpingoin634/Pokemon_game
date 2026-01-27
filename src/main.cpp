8c012cf695899a945da7105e3d0fbfa3cc3aa192
Louka <louka.avenia@gmail.com>
2026-01-27 12:05:41 +0100

    Initialisation de la fenêtre SFML (Blue Screen Test)

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,20 @@
 // main.cpp - Test SFML 3.0.2
-#include <SFML/Graphics.hpp>
+#include "../include/SFML/Graphics.hpp" // On pointe vers le dossier local
 
 int main() {
-    // Création de la fenêtre
+    // Création de la fenêtre (Syntaxe SFML 3.0)
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Projet Pokemon Binome");
 
     while (window.isOpen()) {
-        // Gestion des événements (fermeture)
+        // Gestion des événements
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
 
-        // Affichage d'un fond bleu (pour prouver que ça marche)
-        window.clear(sf::Color::Blue);
+        // Fond bleu Pokémon !
+        window.clear(sf::Color(100, 149, 237)); // Un bleu un peu plus doux (Cornflower Blue)
         window.display();
     }
     return 0;
